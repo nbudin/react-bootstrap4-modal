9d5cb4fa4d26bcf2ccd2a3a45ae3df46e3720dac
Nat Budin <nbudin@patientslikeme.com>
2017-07-15 17:44:52 -0700

    Refactor a little; add ConfirmModal

---
+++ src/ConfirmModal.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Modal from './index';
+
+const ConfirmModal = ({ visible, onOK, onCancel, children }) => (
+  <Modal visible={visible}>
+    <div className="modal-header">
+      <h5 className="modal-title">Confirmation</h5>
+    </div>
+    <div className="modal-body">
+      {children}
+    </div>
+    <div className="modal-footer">
+      <button type="button" className="btn btn-secondary" onClick={onCancel}>
+        Cancel
+      </button>
+      <button type="button" className="btn btn-primary" onClick={onOK}>
+        OK
+      </button>
+    </div>
+  </Modal>
+);
+
+ConfirmModal.propTypes = {
+  children: PropTypes.node.isRequired,
+  visible: PropTypes.bool.isRequired,
+  onOK: PropTypes.func.isRequired,
+  onCancel: PropTypes.func.isRequired,
+};
+
+export default ConfirmModal;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import ConfirmModal from './ConfirmModal';
+import Modal from './Modal';
+
+export { ConfirmModal };
+export default Modal;
