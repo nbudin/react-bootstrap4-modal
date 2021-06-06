ffd34f9069a717ac81d74a397d807a5058df5b11
Nat Budin <natbudin@gmail.com>
2021-06-06 12:13:21 -0700

    TypeScript, ESM, tree shaking, function components

---
+++ src/ConfirmModal.tsx
@@ -0,0 +1,50 @@
+import React, { ReactNode } from 'react';
+import Modal from './index';
+
+export type ConfirmModalProps = {
+  children: ReactNode;
+  visible: boolean;
+  onOK: React.MouseEventHandler<HTMLButtonElement>;
+  onCancel: React.MouseEventHandler<HTMLButtonElement>;
+  /** Defaults to false */
+  disableButtons?: boolean;
+  /** Defaults to "OK" */
+  okText?: string;
+  /** Defaults to "Cancel" */
+  cancelText?: string;
+};
+
+function ConfirmModal({
+  visible,
+  onOK,
+  onCancel,
+  children,
+  disableButtons = false,
+  okText = 'OK',
+  cancelText = 'Cancel',
+  ...other
+}: ConfirmModalProps): JSX.Element {
+  return (
+    <Modal visible={visible} {...other}>
+      <div className="modal-header">
+        <h5 className="modal-title">Confirmation</h5>
+      </div>
+      <div className="modal-body">{children}</div>
+      <div className="modal-footer">
+        <button
+          type="button"
+          className="btn btn-secondary"
+          onClick={onCancel}
+          disabled={disableButtons}
+        >
+          {cancelText}
+        </button>
+        <button type="button" className="btn btn-primary" onClick={onOK} disabled={disableButtons}>
+          {okText}
+        </button>
+      </div>
+    </Modal>
+  );
+}
+
+export default ConfirmModal;
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import ConfirmModal from './ConfirmModal';
-import Modal from './Modal';
-
-export { ConfirmModal };
-export default Modal;
